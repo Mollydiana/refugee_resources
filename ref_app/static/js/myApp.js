dad8972d82c3212709e83ff10ba0e487ac7ec516
Molly <Mollydiana@gmail.com>
2014-11-14 17:32:39 -0800

    modals started

--- ref_app/static/js/myApp.js
+++ ref_app/static/js/myApp.js
@@ -3,9 +3,10 @@ var app = angular.module('myApp', ["ngRoute", "mobile-angular-ui"]);
     app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/', {templateUrl: '/static/js/views/home.html', controller: homeController}).
-//        when('/language/', {templateUrl: '/static/js/views/language.html', controller: languageController}).
+        when('/language/', {templateUrl: '/static/js/views/language.html', controller: languageController}).
 //        when('/housing/', {templateUrl: '/static/js/views/housing.html', controller: housingController}).
 //        when('/help/', {templateUrl: '/static/js/views/help.html', controller: helpController}).
-//        when('/health/', {templateUrl: '/static/js/views/health.html', controller: healthController}).
+        when('/health/', {templateUrl: '/static/js/views/health.html', controller: healthController}).
+        when('/modal/', {templateUrl: '/static/js/views/modal.html', controller: function() {window.ModalEffects()}}).
         otherwise({redirectTo: '/'});
 }]);
