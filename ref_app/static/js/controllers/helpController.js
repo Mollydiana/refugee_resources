89bec12f125850c96d19bed574f4923b6af6b0d3
Molly <Mollydiana@gmail.com>
2014-11-15 22:08:12 -0800

    three buttons working with functionality

--- ref_app/static/js/controllers/helpController.js
+++ ref_app/static/js/controllers/helpController.js
@@ -1,3 +1,23 @@
-function helpController($scope) {
+function helpController($scope, $firebase) {
+    var ref = new Firebase("https://refugeeresources.firebaseio.com/");
+    $scope.messages = $firebase(ref).$asArray();
+    function updateScroll(){
+        $('#example-message').scrollTop($('#example-message')[0].scrollHeight());
+    }
+
+    //ADD MESSAGE METHOD
+    $scope.addMessage = function (e) {
+
+        //LISTEN FOR RETURN KEY
+        if (e.keyCode === 13 && $scope.msg) {
+            //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+            var name = $scope.name || 'anonymous';
+            $scope.messages.$add({from: name, body: $scope.msg});
+            //RESET MESSAGE
+            $scope.msg = "";
+            updateScroll();
+
+        }
+    }
 
 }
--- ref_app/static/js/myApp.js
+++ ref_app/static/js/myApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ["ngRoute", "mobile-angular-ui"]);
+var app = angular.module('myApp', ["ngRoute", "firebase", "mobile-angular-ui"]);
 
     app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
