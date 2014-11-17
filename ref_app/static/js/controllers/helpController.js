dae7c05f927ae7d14987838f0cfc9a1b1d88c545
Molly <Mollydiana@gmail.com>
2014-11-17 11:06:55 -0800

    homeController CSS updated

--- ref_app/static/js/controllers/helpController.js
+++ ref_app/static/js/controllers/helpController.js
@@ -1,6 +1,8 @@
 function helpController($scope, $firebase) {
     var ref = new Firebase("https://refugeeresources.firebaseio.com/");
     $scope.messages = $firebase(ref).$asArray();
+    $scope.username = "guest" + Math.floor(Math.random() * 101);
+
     function updateScroll(){
         $('#example-message').scrollTop($('#example-message')[0].scrollHeight());
     }
@@ -11,12 +13,11 @@ function helpController($scope, $firebase) {
         //LISTEN FOR RETURN KEY
         if (e.keyCode === 13 && $scope.msg) {
             //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-            var name = $scope.name || 'anonymous';
+            var name = $scope.name || $scope.username;
             $scope.messages.$add({from: name, body: $scope.msg});
             //RESET MESSAGE
             $scope.msg = "";
             updateScroll();
-
         }
     }
 
