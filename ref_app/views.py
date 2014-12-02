bf7b488e0d856a1537f75c5b618d48f25efefc2b
Molly <Mollydiana@gmail.com>
2014-12-01 18:46:30 -0800

    working on respinsiveness

--- ref_app/views.py
+++ ref_app/views.py
@@ -9,20 +9,20 @@ def index(request):
 
 # Adding csrf_exempt else Django gives 403 Forbidden and does not hit proxy_to()
 # I hope you check this comment in
-def proxy_to(request, path, target_url):
-    url = '%s%s' % (target_url, path)
-    headers = {
-        # 'Authorization': 'Basic TW9sbHlkaWFuYUBnbWFpbC5jb206MUYxZGRsZXI=',
-        # 'User-Agent': 'Mollydiana@gmail.com',
-        # 'Content-Type': 'application/json'
-    }
-    if request.method == 'GET':
-        proxied_response = requests.get(url, headers=headers)
-    elif request.method == 'POST':
-        proxied_response = requests.post(url, data=request.body, headers=headers)
-    elif request.method == 'PUT':
-        proxied_response = requests.put(url, data=request.body, headers=headers)
-    elif request.method == 'DELETE':
-        proxied_response = requests.delete(url, data=request.body, headers=headers)
-
-    return HttpResponse(proxied_response)
+# def proxy_to(request, path, target_url):
+#     url = '%s%s' % (target_url, path)
+#     # headers = {
+#         # 'Authorization': 'Basic TW9sbHlkaWFuYUBnbWFpbC5jb206MUYxZGRsZXI=',
+#         # 'User-Agent': 'Mollydiana@gmail.com',
+#         # 'Content-Type': 'application/json'
+#     # }
+#     if request.method == 'GET':
+#         proxied_response = requests.get(url, headers=headers)
+#     elif request.method == 'POST':
+#         proxied_response = requests.post(url, data=request.body, headers=headers)
+#     elif request.method == 'PUT':
+#         proxied_response = requests.put(url, data=request.body, headers=headers)
+#     elif request.method == 'DELETE':
+#         proxied_response = requests.delete(url, data=request.body, headers=headers)
+#
+#     return HttpResponse(proxied_response)
